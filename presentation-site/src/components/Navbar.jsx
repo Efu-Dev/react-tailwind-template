bf096e31254105d035a01394ac7c5ad84c62fa75
Diego Faria <diegoafbrito12@gmail.com>
2022-08-22 19:09:18 -0400

    added mobile menu

--- presentation-site/src/components/Navbar.jsx
+++ presentation-site/src/components/Navbar.jsx
@@ -29,7 +29,16 @@ const Navbar = () => {
         <div
           className={`${toggle? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}        
         >
-
+          <ul className='list-none flex-col justify-end items-center flex-1'>
+            {navLinks.map((navLink, index) => (
+              <li key={navLink.id} className={`font-poppins font-normal cursor-pointer 
+              text-[16px] text-white ${index === navLinks.length - 1 ? 'mb-0' : 'mb-4'}`}>
+                <a href={`#${navLink.id}`}>
+                  {navLink.title}
+                </a>
+              </li>
+            ))}        
+          </ul>
         </div>
       </div>
     </nav>
