b7f6f23f744dfe27682779aa9141a63d22a82647
Diego Faria <diegoafbrito12@gmail.com>
2022-08-24 16:48:21 -0400

    added remaining information to Hero

--- presentation-site/src/components/GetInTouch.jsx
+++ presentation-site/src/components/GetInTouch.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from '../style.js';
 import {arrowUp} from '../assets';
 
+// TODO needs hovering styles
 const GetInTouch = () => (
   <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
     <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
--- presentation-site/src/components/Hero.jsx
+++ presentation-site/src/components/Hero.jsx
@@ -19,14 +19,34 @@ const Hero = () => {
         <div className='flex flex-row justify-between items-center w-full'>
           <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
             The Next <br className='sm:block hidden' /> 
-            <span className='text-gradient'>Generation</span><br />
-            Payment Method
+            <span className='text-gradient'>Generation</span>
           </h1>
 
           <div className='ss:flex hidden md:mr-4 mr-0'>
             <GetInTouch />
           </div>
-        </div>   
+        </div>
+
+        <h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
+            Payment Method
+        </h1>
+
+        <p className={`${styles.paragraph} max-w-[470px] mt-5 mb-5`}>
+          Hello! Here goes the text you want to apply for this template.
+          I am going to use it myself (edited) for my future portfolio, but feel
+          free to use on your own projects! Cheers!
+        </p>
+      </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className='w-[100%] h-[100%] rel z-[5]' />
+        <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
+        <div className='absolute z-[1] rounded-full bottom-40 w-[50%] h-[80%] white__gradient' />
+        <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetInTouch />
       </div>
     </section>
   );
