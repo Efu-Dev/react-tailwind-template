af8f5f24afaf6463241593f70a1cfc8d3745ec40
Diego Faria <diegoafbrito12@gmail.com>
2022-08-30 16:13:18 -0400

    completed Business component

--- presentation-site/src/components/Business.jsx
+++ presentation-site/src/components/Business.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import {features} from '../constants';
+import styles, {layout} from '../style.js';
+import Button from './Button.jsx';
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business,<br className='sm:block hidden' />
+          we'll handle the money. 
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu 
+          Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu Jammu 
+        </p>
+        <Button />
+      </div>
+    </section>
   );
 };
 
